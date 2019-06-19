930e93cad82e30109994d4e6c3688f4636775127
mhaji <minhajhaji2019@u.northwestern.edu>
2019-06-19 16:01:29 -0500

    first commit

---
+++ keys.py
@@ -0,0 +1,4 @@
+api_key = "19b5c76df39770414a935f1f08e0edee"
+
+api_key1 = "aa50c59aae942df91614c8d6cf23a528"
+
---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+
+api_key = "7f82f14544a9b3a7b6f9b24b5b56aebd"
+
+api_key1 = "aa50c59aae942df91614c8d6cf23a528"
